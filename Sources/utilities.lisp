20a5debdf43506dc6b54ed72a764b8d40dccfcf1
Charles K. Neimog <charlesneimog@outlook.com>
2022-06-24 09:34:53 -0300

    first commit

---
+++ Sources/utilities.lisp
@@ -0,0 +1,262 @@
+(in-package :om)
+
+;===================================================================== Files control =====================================
+
+(defun get-filename (x)
+"I forgot the name of the function that I stolen somewhere."
+
+(let* (
+      (filename (namestring x))
+      (file (om::string-to-list filename "\\")))
+  (car (last file))))   
+
+;=====================================================================  
+
+(defun search-inside-some-folder (folder extension)                                                                         
+      (let* (
+            (thepath folder)
+            (thefilelist (om-directory thepath 
+                              :type extension
+                              :directories t 
+                              :files t 
+                              :resolve-aliases nil 
+                              :hidden-files nil))
+            (more-folders? (mapcar (lambda (x) (if 
+                                                      (system::directory-pathname-p x)
+                                                      (search-inside-some-folder x extension)
+                                                      x)) thefilelist)))
+            more-folders?))
+
+;=====================================================================  
+
+(defun list->string-fun (ckn-list)
+  (when ckn-list
+    (concatenate 'string 
+                 (write-to-string (car ckn-list)) (list->string-fun (cdr ckn-list)))))
+
+
+;; ======================================
+(defun concatString (list)
+  "A non-recursive function that concatenates a list of strings."
+  (if (listp list)
+      (let ((result ""))
+        (dolist (item list)
+          (if (stringp item)
+              (setq result (concatenate 'string result item))))
+        result)))
+        
+      ;; https://stackoverflow.com/questions/5457346/lisp-function-to-concatenate-a-list-of-string
+
+;=====================================================================  
+
+
+
+; ====================================================================================================
+; ====================================================================================================
+; ================================= OM-KEYS ==========================================================
+; ====================================================================================================
+; ====================================================================================================
+
+(defmethod editor-key-action ((editor patch-editor) key)
+  (declare (special *general-player*))
+
+  (let* ((panel (get-editor-view-for-action editor))
+         (selected-boxes (get-selected-boxes editor))
+         (selected-connections (get-selected-connections editor))
+         (player-active (and (boundp '*general-player*) *general-player*)))
+
+(when panel
+
+      (case key
+
+        ;;; play/stop commands
+        (#\Space (when player-active (play/stop-boxes selected-boxes)))
+        (#\s (when player-active (stop-boxes selected-boxes)))
+
+        (:om-key-delete (unless (edit-lock editor)
+                          (store-current-state-for-undo editor)
+                          (remove-selection editor)))
+
+        (#\n (if selected-boxes
+                 (mapc 'set-show-name selected-boxes)
+               (unless (edit-lock editor)
+                 (make-new-box panel))))
+
+        (#\p (unless (edit-lock editor)
+               (make-new-abstraction-box panel)))
+
+        
+        (:om-key-left (unless (edit-lock editor)
+                        (if (om-option-key-p)
+                            (when selected-boxes
+                              (store-current-state-for-undo editor)
+                              (mapc 'optional-input-- selected-boxes))
+                          (let ((selection (or selected-boxes selected-connections)))
+                            (store-current-state-for-undo editor :action :move :item selection)
+                            (mapc
+                             #'(lambda (f) (move-box f (if (om-shift-key-p) -10 -1) 0))
+                             selection))
+                          )))
+        (:om-key-right (unless (edit-lock editor)
+                         (if (om-option-key-p)
+                             (when selected-boxes
+                               (store-current-state-for-undo editor)
+                               (mapc 'optional-input++ selected-boxes))
+                           (let ((selection (or selected-boxes selected-connections)))
+                             (store-current-state-for-undo editor :action :move :item selection)
+                             (mapc #'(lambda (f) (move-box f (if (om-shift-key-p) 10 1) 0))
+                                   selection))
+                           )))
+        (:om-key-up (unless (edit-lock editor)
+                      (store-current-state-for-undo editor :action :move :item (or selected-boxes selected-connections))
+                      (mapc #'(lambda (f) (move-box f 0 (if (om-shift-key-p) -10 -1)))
+                            (or selected-boxes selected-connections))
+                      ))
+        (:om-key-down (unless (edit-lock editor)
+                        (store-current-state-for-undo editor :action :move :item (or selected-boxes selected-connections))
+                        (mapc #'(lambda (f) (move-box f 0 (if (om-shift-key-p) 10 1)))
+                              (or selected-boxes selected-connections))
+                        ))
+
+        (#\k (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'keyword-input++ selected-boxes))))
+        (#\+ (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'keyword-input++ selected-boxes))))
+        (#\K (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'keyword-input-- selected-boxes))))
+        (#\- (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'keyword-input-- selected-boxes))))
+
+        (#\> (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'optional-input++ selected-boxes))))
+        (#\< (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'optional-input-- selected-boxes))))
+
+        (#\b (when selected-boxes
+               (store-current-state-for-undo editor)
+               (mapc 'switch-lock-mode selected-boxes)))
+
+       (#\c (unless (edit-lock editor)
+                     (store-current-state-for-undo editor)
+                     (if selected-boxes
+                            (auto-connect-box selected-boxes editor panel)
+                            (make-new-comment panel))))
+
+       ;; ================================================================
+
+        (#\1 (unless (or (edit-lock editor) (get-pref-value :general :auto-ev-once-mode))
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'switch-evonce-mode selected-boxes))))
+
+        (#\l (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'switch-lambda-mode selected-boxes))))
+
+        (#\m (mapc 'change-display selected-boxes))
+
+
+        ;;; Box editing
+        ;;; => menu commands ?
+
+        (#\A (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (align-selected-boxes editor)))
+
+        (#\S (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (let ((selection (append selected-boxes selected-connections)))
+                 (mapc 'consolidate-appearance selection)
+                 (update-inspector-for-editor editor nil t))))
+
+        (#\c (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (if selected-boxes
+                   (auto-connect-box selected-boxes editor panel)
+                 (make-new-comment panel))))
+
+        (#\C (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (auto-connect-seq selected-boxes editor panel)))
+
+        (#\r (unless (edit-lock editor)
+                     (store-current-state-for-undo editor)
+                     (mapc 'set-reactive-mode (or selected-boxes selected-connections))))
+
+        (#\i (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (mapc 'initialize-size (or selected-boxes selected-connections))))
+
+        (#\I (mapc 'initialize-box-value selected-boxes))
+
+        (#\r (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (mapc 'set-reactive-mode (or selected-boxes selected-connections))))
+
+        ;;; abstractions
+        (#\a (unless (edit-lock editor)
+               (when selected-boxes
+                 (store-current-state-for-undo editor)
+                 (mapc 'internalize-abstraction selected-boxes))))
+
+        (#\E (unless (edit-lock editor)
+               (encapsulate-patchboxes editor panel selected-boxes)))
+
+        (#\U (unless (edit-lock editor)
+               (unencapsulate-patchboxes editor panel selected-boxes)))
+
+        (#\L (unless (edit-lock editor)
+               (store-current-state-for-undo editor)
+               (list-boxes editor panel selected-boxes)))
+
+        (#\v (eval-editor-boxes editor selected-boxes))
+
+        (#\w (om-debug))
+
+        (#\h (funcall (help-command editor)))
+
+        (#\d (when selected-boxes
+               (mapcar #'print-help-for-box selected-boxes)))
+
+        
+        ; ======================================== 
+        ; ========== OM-PY =======================
+        ; ========================================
+
+       (#\z (if   (and  selected-boxes 
+                        (find-library "OM-py") 
+                        (or 
+                              (equal (type-of (car (om::list! selected-boxes))) 'omboxpy) 
+                              (equal (type-of (car (om::list! selected-boxes))) 'OMBox-run-py)))
+
+              ; if omboxpy or OMBox-run-py is selected, then open the VS-code
+
+                  (progn
+                     (om::om-print "Opening VScode!" "OM-Py")
+                     (defparameter *vscode-opened* t)
+                     (open-vscode selected-boxes))
+
+              ; if nothing is selected, then new py-script
+
+                  (unless (edit-lock editor)
+                            ;(print panel)
+                            (make-new-py-box panel))))
+        
+       ; ======================================== 
+       ; ========== OM-PY =======================
+       ; ======================================== 
+        
+       (otherwise nil)))))
